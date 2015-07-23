66836087bed839df4b0972ce730a1f8dbee41bbc
shawn <shawn@shawns-Mac.local>
2015-07-22 20:07:06 -0700

    add pods

---
+++ zhihu.study/zhihu.study/LaunchImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchImageViewController.swift
+//  zhihu.study
+//
+//  Created by shawn on 22/7/15.
+//  Copyright (c) 2015 shawn. All rights reserved.
+//
+
+import UIKit
+
+class LaunchImageViewController: UIViewController
+{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+}
